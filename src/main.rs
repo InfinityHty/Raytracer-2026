021c897e6f74d6e5457cd1328ac1a4b7d5b34067
InfinityHty <infinityhty.sjtu.edu.cn>
2026-07-07 19:38:16 +0800

    Image2 in book1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
-use console::style;
-use image::{ImageBuffer, RgbImage};
-use indicatif::ProgressBar;
-
+use console::style;// 给控制台打印的文字加颜色、加粗等样式
+use image::{ImageBuffer, Rgb, RgbImage};// Rust最主流的图像处理库 创建画布 逐像素绘制光线追踪结果 导出文件图片
+use indicatif::ProgressBar;// 进度条可视化
+mod vec3;
+use vec3::Vec3;
+mod ray;
+use ray::Ray;
 fn main() {
-    let path = std::path::Path::new("output/book1/image1.png");
+    // 创建保存路径
+    let path = std::path::Path::new("output/book1/image2.png");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
+    // 设置eye point viewport等等参数
+    let aspect_ration = 16.0 / 9.0;
+    let eye_point : Vec3 = Vec3::new(0.0,0.0,0.0);
+    let f = 1.0;
 
-    let width = 256;
-    let height = 256;
+    let width = 400;
+    let height = (width as f64 / aspect_ration) as u32;
+
+    let viewport_height = 2.0;
+    let viewport_width = (viewport_height * (width as f64 / height as f64));
+
+    let viewport_v = Vec3::new(0.0,-viewport_height,0.0);
+    let viewport_u = Vec3::new(viewport_width as f64,0.0,0.0);
+
+    let pixel_u = viewport_u / width as f64;
+    let pixel_v = viewport_v / height as f64;
+
+    let upper_left = eye_point - Vec3::new(0.0,0.0,f) - viewport_u / 2.0 -viewport_v / 2.0;
+    let pixel_00 = upper_left + pixel_u * 0.5 + pixel_v * 0.5;
     // different from the book, we use image crate to create a .png image rather than outputting .ppm file, which is not widely used.
     // anyway, you may output any image format you like.
     let mut img: RgbImage = ImageBuffer::new(width, height);
@@ -19,13 +39,15 @@ fn main() {
         ProgressBar::new((height * width) as u64)
     };
 
-    for j in (0..height).rev() {
+    for j in 0..height {
         for i in 0..width {
+            let pixel_ij = pixel_00 + pixel_u * i as f64 + pixel_v * j as f64;
+            // println!("{}",pixel_ij.get_y());
+            let ray_direction = pixel_ij - eye_point;
+            let ray = Ray::new(&eye_point,&ray_direction);
             let pixel = img.get_pixel_mut(i, j);
-            let r: f64 = (i as f64) / ((width - 1) as f64) * 255.999;
-            let g: f64 = (j as f64) / ((height - 1) as f64) * 255.999;
-            let b: f64 = 0.25 * 255.999;
-            *pixel = image::Rgb([r as u8, g as u8, b as u8]);
+
+            *pixel = ray_color(ray);
         }
         progress.inc(1);
     }
@@ -36,4 +58,12 @@ fn main() {
         style(path.to_str().unwrap()).yellow()
     );
     img.save(path).expect("Cannot save the image to the file");
+}
+
+fn ray_color(ray: Ray) -> Rgb<u8> {
+    let unit_direction = ray.direction().normalize();
+    let a = (unit_direction.get_y() + 1.0) * 0.5;
+    let mut color = Vec3::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a;
+    color = color * 256.0;
+    color.to_rgb()
 }
