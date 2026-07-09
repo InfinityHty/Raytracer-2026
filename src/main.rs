d9e63ac40fc4c5c6f310d516cb9b3ec0d82b69e7
InfinityHty <infinityhty.sjtu.edu.cn>
2026-07-09 09:39:58 +0800

    Image 3

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod ray;
 use ray::Ray;
 fn main() {
     // 创建保存路径
-    let path = std::path::Path::new("output/book1/image2.png");
+    let path = std::path::Path::new("output/book1/image3.png");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
     // 设置eye point viewport等等参数
@@ -29,6 +29,7 @@ fn main() {
 
     let upper_left = eye_point - Vec3::new(0.0, 0.0, f) - viewport_u / 2.0 - viewport_v / 2.0;
     let pixel_00 = upper_left + pixel_u * 0.5 + pixel_v * 0.5;
+    let center_of_sphere = Vec3::new(0.0, 0.0, -f);
     // different from the book, we use image crate to create a .png image rather than outputting .ppm file, which is not widely used.
     // anyway, you may output any image format you like.
     let mut img: RgbImage = ImageBuffer::new(width, height);
@@ -47,7 +48,7 @@ fn main() {
             let ray = Ray::new(eye_point, ray_direction);
             let pixel = img.get_pixel_mut(i, j);
 
-            *pixel = ray_color(ray);
+            *pixel = ray_color(&ray, &center_of_sphere);
         }
         progress.inc(1);
     }
@@ -59,10 +60,24 @@ fn main() {
     );
     img.save(path).expect("Cannot save the image to the file");
 }
-fn ray_color(ray: Ray) -> Rgb<u8> {
-    let unit_direction = ray.direction().normalize();
-    let a = (unit_direction.get_y() + 1.0) * 0.5;
-    let mut color = Vec3::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a;
-    color = color * 256.0;
-    color.to_rgb()
+fn ray_color(ray: &Ray, center: &Vec3) -> Rgb<u8> {
+    if hit_sphere(*center, 0.5, ray) {
+        let color = vec3::Vec3::new(256.0, 0.0, 0.0);
+        color.to_rgb()
+    } else {
+        let unit_direction = ray.direction().normalize();
+        let a = (unit_direction.get_y() + 1.0) * 0.5;
+        let mut color = Vec3::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a;
+        color = color * 256.0;
+        color.to_rgb()
+    }
+}
+fn hit_sphere(center: Vec3, radius: f64, ray: &Ray) -> bool {
+    let a = ray.direction().length_squared();
+    let b = ray.direction() * (center - ray.ori()) * -2.0;
+    let c = (center - ray.ori()).length_squared() - radius * radius;
+    if b * b - 4.0 * a * c < 0.0 {
+        return false;
+    }
+    true
 }
