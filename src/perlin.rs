70bc61bbb2786d6b7697acc438492b30c30046e8
InfinityHty <infinityhty.sjtu.edu.cn>
2026-07-14 09:42:27 +0800

    Book2 Image10

--- src/perlin.rs
+++ src/perlin.rs
@@ -27,11 +27,41 @@ impl PerlinNoise {
         }
     }
     pub fn noise(&self, point: &Vec3) -> f64 {
-        let i = (((point.x * 4.0) as i32) & 255) as usize;
-        let j = (((point.y * 4.0) as i32) & 255) as usize;
-        let k = (((point.z * 4.0) as i32) & 255) as usize;
+        let u = point.x - point.x.floor();
+        let v = point.y - point.y.floor();
+        let w = point.z - point.z.floor();
 
-        self.random_float[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let i = point.x.floor() as i32;
+        let j = point.y.floor() as i32;
+        let k = point.z.floor() as i32;
+        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+        #[allow(clippy::needless_range_loop)]
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.random_float[self.perm_x
+                        [((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]];
+                }
+            }
+        }
+        self.trilinear_interp(c, u, v, w)
+    }
+    fn trilinear_interp(&self, c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accumulation = 0.0;
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accumulation += (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accumulation
     }
     fn random(min: f64, max: f64) -> f64 {
         let mut rng = rng();
